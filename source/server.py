091689842afb945344090a24f89dc83217069473
Hüseyin Tutan <109728044+huseyintutan@users.noreply.github.com>
2024-05-14 21:14:12 +0300

    gui lagging fixed

--- source/server.py
+++ source/server.py
@@ -5,23 +5,40 @@ from udpclient import UdpSocket
 
 app = Flask(__name__)
 
-gps_data = "No data received yet."
+gps_data1 = "No data received yet."
+gps_data2 = "No data received yet."
+gps_data3 = "No data received yet."
 
 def listen_for_gps_data():
-    global gps_data
+    global gps_data1
+    global gps_data2
+    global gps_data3
+
     udp_socket = UdpSocket()
     while True:
-        gps_data = udp_socket.recv()
+        gps_data1 = udp_socket.recv_from_port1()
+        gps_data2 = udp_socket.recv_from_port2()
+        gps_data3 = udp_socket.recv_from_port3()
         #print(gps_data)
 
 @app.route('/')
 def index():
-    return render_template('dashboard.html', gps_data=gps_data)
+    return render_template('dashboard.html', gps_data1=gps_data1,gps_data2=gps_data2,gps_data3=gps_data3)
 
-@app.route('/get_gps_data')
-def get_gps_data():
+@app.route('/get_data1')
+def get_gps_data1():
 
-    return gps_data
+    return gps_data1
+
+@app.route('/get_data2')
+def get_gps_data2():
+
+    return gps_data2
+
+@app.route('/get_data3')
+def get_gps_data3():
+
+    return gps_data3
 
 if __name__ == '__main__':
     thread = threading.Thread(target=listen_for_gps_data)
