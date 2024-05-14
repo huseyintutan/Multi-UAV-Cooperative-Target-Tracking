d1a46feb1e5eaa4fc881820ecbcf3f4956098927
Hüseyin Tutan <109728044+huseyintutan@users.noreply.github.com>
2024-05-14 21:13:32 +0300

    udp server updated for 3 aircraft struture

--- source/udpclient.py
+++ source/udpclient.py
@@ -1,23 +1,42 @@
 import socket
 
 class UdpSocket:
-    def __init__(self, host='localhost', port=1234):
+    def __init__(self, host='localhost', port1=1234, port2=1235, port3=1236):
         self.host = host
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind((self.host, self.port))
+        self.port1 = port1
+        self.port2 = port2
+        self.port3 = port3
+        self.sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock1.bind((self.host, self.port1))
+        self.sock2.bind((self.host, self.port2))
+        self.sock3.bind((self.host, self.port3))
 
-    def recv(self):
-        return self.sock.recv(1024).decode()
+    def recv_from_port1(self):
+        return self.sock1.recv(1024).decode()
+
+    def recv_from_port2(self):
+        return self.sock2.recv(1024).decode()
+    
+    def recv_from_port3(self):
+        return self.sock3.recv(1024).decode()
 
     def close(self):
-        self.sock.close()
+        self.sock1.close()
+        self.sock2.close()
+        self.sock3.close()
 
 
 if __name__ == '__main__':
     udp_socket = UdpSocket()
 
-    while True:
-        print(udp_socket.recv())
-
-    udp_socket.close()  
+    try:
+        while True:
+            print("Port 1234:", udp_socket.recv_from_port1())
+            print("Port 1235:", udp_socket.recv_from_port2())
+            print("Port 1236:", udp_socket.recv_from_port3())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        udp_socket.close()
